d88453777cfb9624576da1e1cad0887165ad7552
David Cooper <david.cooper@unit21.ai>
2024-04-07 14:15:51 -0700

    udpated to call async methods

--- libadd.go
+++ libadd.go
@@ -1,11 +1,33 @@
-//libadd.go
+// libadd.go
 package main
 
 import "C"
 
+import (
+	"fmt"
+	"time"
+)
+
 //export add
 func add(left, right int) int {
+	time.Sleep(500 * time.Millisecond)
 	return left + right
+}
+
+//export async_add
+func async_add() {
+	for i := 0; i < 5; i++ {
+		go add(1, 2)
+		fmt.Println(fmt.Sprintf("Async Iteration %d", i))
+	}
+}
+
+//export slow_add
+func slow_add() {
+	for i := 0; i < 5; i++ {
+		add(1, 2)
+		fmt.Println(fmt.Sprintf("Slow Iteration %d", i))
+	}
 }
 
 func main() {
